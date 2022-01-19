77bd9711a46c1fb2087a486fce5a050e7a840c94
Felipe Vicente Romano <felipe-romano@uol.com.br>
2022-01-19 17:42:14 -0300

    Site evolved

--- js/script.js
+++ js/script.js
@@ -1,5 +1,39 @@
 $(document).ready(function(){
 
-	$('#slides').superslides();
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Game Developer.", "Game Designer."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor:false
+	});
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 8,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	        	items:3
+	        },
+	        938:{
+	            items:4
+	        },
+	        1040:{
+	        	items: 5
+	        }
+	    }
+	});
 
 });

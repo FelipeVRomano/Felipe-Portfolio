713d5e8d411ddb4f87e22e0946c1025af92814e1
Felipe Vicente Romano <felipe-romano@uol.com.br>
2022-01-22 10:14:36 -0300

    loading

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+$(window).on("load", function(){
+
+	$(".loader .inner").fadeOut(500, function() {
+		$(".loader").fadeOut(750);
+	});
+
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue:false
+		}
+	});
+})
+
 $(document).ready(function(){
 
 	$('#slides').superslides({
@@ -37,15 +53,6 @@ $(document).ready(function(){
 	});
 
 	$("[data-fancybox]").fancybox();
-
-	$(".items").isotope({
-		filter: '*',
-		animationOptions: {
-			duration: 1500,
-			easing: 'linear',
-			queue:false
-		}
-	});
 
 	$("#filters a").click(function(){
 		$("filters.current").removeClass("current");
